b60838b39089577626ae4d4da5a5dbb5463a6786
jackHay22 <jack@allspice.io>
2022-07-11 14:45:48 -0400

    add unit tests for duplicate creation, removal

--- models/organization/team_invite_test.go
+++ models/organization/team_invite_test.go
@@ -25,3 +25,34 @@ func TestTeam_EmailExists(t *testing.T) {
 	_, err := organization.CreateTeamInvite(db.DefaultContext, user2, team, "user2@example.com")
 	assert.Error(t, err)
 }
+
+func TestTeam_Invite(t *testing.T) {
+	assert.NoError(t, unittest.PrepareTestDatabase())
+
+	team := unittest.AssertExistsAndLoadBean(t, &organization.Team{ID: 2}).(*organization.Team)
+	user1 := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 1}).(*user_model.User)
+
+	_, err := organization.CreateTeamInvite(db.DefaultContext, user1, team, "user3@example.com")
+	assert.NoError(t, err)
+
+	// Shouldn't allow duplicate invite
+	_, err = organization.CreateTeamInvite(db.DefaultContext, user1, team, "user3@example.com")
+	assert.Error(t, err)
+}
+
+func TestTeam_RemoveInvite(t *testing.T) {
+	assert.NoError(t, unittest.PrepareTestDatabase())
+
+	team := unittest.AssertExistsAndLoadBean(t, &organization.Team{ID: 2}).(*organization.Team)
+	user1 := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 1}).(*user_model.User)
+
+	invite, err := organization.CreateTeamInvite(db.DefaultContext, user1, team, "user5@example.com")
+	assert.NoError(t, err)
+
+	// should remove invite
+	assert.NoError(t, organization.RemoveInviteByID(db.DefaultContext, invite.ID, invite.TeamID))
+
+	// invite should not exist
+	_, err = organization.GetInviteByToken(db.DefaultContext, invite.Token)
+	assert.Error(t, err)
+}
